36a2635ddb4ddb00b2b487c41ab0295d6084d5e6
Baráth Barnabás <barnibarath@gmail.com>
2024-09-26 11:44:45 +0200

    Employers, Jobs table added with make:model --all

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -8,9 +8,9 @@
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag class="size-small">Backend</x-tag>
-            <x-tag class="size-small">Frontend</x-tag>
-            <x-tag class="size-small">Manager</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
 
         <x-employer-logo :width="42"></x-employer-logo>
